9035a8ce864e26fbdd5cc94f09c6736b68c38d2e
kaduceusxx <kaduceusx@gmail.com>
2021-11-28 12:53:24 +0100

    Siguiendo curso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
 import { RegisterComponent } from './components/inicio/register/register.component';
 import { LoginComponent } from './components/inicio/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 const routes: Routes = [
   {path: 'bienvenidos', component: BienvenidaComponent},
+  {path: 'inicio', component:InicioComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component:LoginComponent},
   {path: '', redirectTo: '/bienvenidos', pathMatch: 'full' },
--- src/app/components/inicio/register/register.component.ts
+++ src/app/components/inicio/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  register!: FormGroup
+
+  constructor(private fb: FormBuilder) { 
+    this.register = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4)] ],
+      confirmaPassword: ['']
+    }, {validator: this.checkPassword});
+  }
 
   ngOnInit(): void {
+  }
+
+  registrarUsuario():void {
+    console.log(this.register);
+  }
+
+  checkPassword(group: FormGroup):any {
+    const pass = group.controls.password.value;
+    const confirmPass = group.controls.confirmaPassword.value;
+
+    return pass === confirmPass ? null : {notSame: true};
   }
 
 }
